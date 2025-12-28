09d0f501bbd8c51c8b4faae68390b22b858ff819
Malte Herrmann <malteherrmann.mail@gmail.com>
2025-12-28 23:33:51 +0100

    add basic logic to check for claude cli and key, cursor-agent cli and open ai key

--- src/llm/mod.rs
+++ src/llm/mod.rs
@@ -3,11 +3,12 @@
 /// supported models.
 use crate::error::LLMError;
 
-use std::env::var;
+use std::{env::var, process::Command};
 
 /// Promptable defines the required functionality
 /// to interact with a language model.
 pub trait Promptable {
+    fn get_name(&self) -> String;
     fn prompt(&self, input: str) -> Result<(), LLMError>;
 }
 
@@ -20,12 +21,45 @@ pub fn get_available_models() -> Result<Vec<String>, LLMError> {
         models.push("anthropic".to_string())
     }
 
+    if check_for_claude() {
+        models.push("claude".to_string())
+    }
+
+    if check_for_cursor_agent() {
+        models.push("cursor-agent".to_string())
+    }
+
+    if check_for_openai_key() {
+        models.push("openai".to_string())
+    }
+
     Ok(models)
 }
 
 fn check_for_anthropic_key() -> bool {
     match var("ANTHROPIC_API_KEY") {
-        Ok(v) => v.is_empty(),
+        Ok(v) => v != "",
+        Err(_) => false,
+    }
+}
+
+fn check_for_claude() -> bool {
+    match Command::new("claude").arg("-h").output() {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn check_for_cursor_agent() -> bool {
+    match Command::new("cursor-agent").arg("-h").output() {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn check_for_openai_key() -> bool {
+    match var("OPENAI_API_KEY") {
+        Ok(v) => v != "",
         Err(_) => false,
     }
 }
